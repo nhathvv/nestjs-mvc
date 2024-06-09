857a5e5c3fc7a2a5f63adec7e0cde2ca459ef8a7
Nhathv21 <Nhathv.21it@gmail.com>
2024-06-09 20:59:37 +0700

    feat: connect database mongodb & configService env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   @Render('home')
   root() {
+    console.log('Port:', this.configService.get<string>('PORT'));
     const result = this.appService.getHello();
     return {
       result,
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT') || 4000;
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
