cf5572c5671a2740eaf18c50b9fa0ef65145aace
Nhathv21 <Nhathv.21it@gmail.com>
2024-06-10 18:33:04 +0700

    feat: add user & define type dto

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -5,13 +5,13 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({ required: true })
   name: string;
 
-  @Prop()
+  @Prop({ required: true })
   email: string;
 
-  @Prop()
+  @Prop({ required: true })
   password: string;
 
   @Prop()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { genSaltSync, hashSync } from 'bcryptjs';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  hashPassword(password: string) {
+    const salt = genSaltSync(10);
+    return hashSync(password, salt);
+  }
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const { email, password, name } = createUserDto;
+    const hashPassword = this.hashPassword(password);
+    const result = this.userModel.create({
+      email,
+      password: hashPassword,
+      name,
+    });
+    return result;
   }
 
   findAll() {
